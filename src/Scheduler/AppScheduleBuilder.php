25fa48f3ac3d3f1b22e475c5415e2bb2a38b271c
rolanddansou <33546430+rolanddansou@users.noreply.github.com>
2026-01-15 02:21:21 +0100

    Update scheduled commands and adjust message handling configuration

--- src/Scheduler/AppScheduleBuilder.php
+++ src/Scheduler/AppScheduleBuilder.php
@@ -12,16 +12,10 @@ class AppScheduleBuilder implements ScheduleBuilder
     {
         $schedule->timezone(new \DateTimeZone(Timezones::UTC));
 
-        /*
-        $schedule->addCommand('app:consume-domain-event')
-            ->description('Consume domain events from the sqlite database and dispatch them to the event bus.')
-            ->everyMinute() // every minute
-            ->withoutOverlapping();
-
-        $schedule->addCommand('app:voucher:expire')
-            ->description('Expire vouchers that have reached their expiration date.')
-            ->cron('0 * * * *') // every hour at minute 0
-            ->withoutOverlapping();
-        */
+        $schedule->addCommand('app:process-async-messages')
+            ->description('Process async messages')
+            ->everyMinute()
+            ->withoutOverlapping()
+        ;
     }
 }
