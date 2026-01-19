bd5c92530078ae6e04624c3f27a3fb12db8a0e20
rolanddansou <33546430+rolanddansou@users.noreply.github.com>
2026-01-19 03:38:28 +0100

    Set default timezone to UTC in index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Feature\Helper\Timezones;
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    date_default_timezone_set(Timezones::UTC);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
