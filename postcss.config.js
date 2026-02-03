7081004c81fd9e8f3936f6390805f9e372694478
rolanddansou <33546430+rolanddansou@users.noreply.github.com>
2026-02-03 02:38:27 +0100

    Refactor service exclusions, update PostCSS configuration, and optimize SCSS structure

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: {
-        //"@tailwindcss/postcss": {},
-        tailwindcss: {},
+        "@tailwindcss/postcss": {},
+        //tailwindcss: {},
         autoprefixer: {},
         ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
     },
